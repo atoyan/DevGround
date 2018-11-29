2ff9639ac9e6c6bbb472537bad3c227877f268ea
WC ando <wc_ando@hotmail.com>
2018-11-29 19:21:52 +0100

    added Redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
 import {BrowserRouter as Router , Route} from 'react-router-dom';
+import {Provider} from "react-redux";
+import store    from "./store";
+
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 
+
+
+
+
 class App extends Component {
+
   render() {
     return (
+      <Provider store = {store}>
       <Router>
       <div className="App">
         <Navbar/> 
@@ -23,6 +32,7 @@ class App extends Component {
       <Footer />
       </div>
       </Router>
+      </Provider>
     );
   }
 } 
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import PropTypes from 'prop-types';
 import classnames from 'classnames';
+import {connect} from 'react-redux';
+import {registerUser} from '../../actions/authActions';
 
  class Register extends Component {
    constructor(){
@@ -26,18 +29,25 @@ import classnames from 'classnames';
         email: this.state.email,
         password: this.state.password,
         password2: this.state.password2
-      }
-        axios.post('/api/users/register', newUser)
-        .then(res=>console.log(res.data))
-        .catch(err=>this.setState({errors: err.response.data}));
+      };
+
+      this.props.registerUser(newUser);
+        // axios.post('/api/users/register', newUser)
+        // .then(res=>console.log(res.data))
+        // .catch(err=>this.setState({errors: err.response.data}));
 
      console.log(newUser);
    }
 
   render() {
+
     const{errors}=this.state;
+    
+    const{user}= this.props.auth; 
+
     return (
      <div className="register">
+     {user?user.name:null}
     <div className="container">
       <div className="row">
         <div className="col-md-8 m-auto">
@@ -71,4 +81,13 @@ import classnames from 'classnames';
   }
 }
 
-export default Register ; 
+Register.protoTypes = {
+  registerUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+}
+
+const mapStateToProps = (state) =>({
+  auth: state.auth
+})
+
+export default connect(mapStateToProps, {registerUser})( Register) ; 
