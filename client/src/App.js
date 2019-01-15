06440716eecd7bc23d927a9099d95c4ecbebd89e
WC ando <wc_ando@hotmail.com>
2019-01-15 15:40:27 +0100

    implement profile reducer and getCurrentProfile action

--- client/src/App.js
+++ client/src/App.js
@@ -1,63 +1,62 @@
-import React, { Component } from 'react';
-import './App.css';
-import {BrowserRouter as Router , Route} from 'react-router-dom';
-import jwt_decode from 'jwt-decode';
-import setAuthToken from './utils/setAuthToken';
-import {setCurrentUser, logoutUser} from './actions/authActions';
-import {Provider} from "react-redux";
-import store    from "./store";
+import React, { Component } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
+import { clearCurrentProfile } from "./actions/profileActions";
+import { Provider } from "react-redux";
+import store from "./store";
 
-import Navbar from './components/layout/Navbar';
-import Footer from './components/layout/Footer';
-import Landing from './components/layout/Landing';
-import Register from './components/auth/Register';
-import Login from './components/auth/Login';
-
-
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import Dashboard from "./components/dashboard/Dashboard";
 
 //Check for Token
-if(localStorage.jwtToken){
+if (localStorage.jwtToken) {
   //Set auth token header off
-    setAuthToken(localStorage.jwt_decode);
+  setAuthToken(localStorage.jwt_decode);
   //Decode Token and get user info and expiration
-    const decoded  =  jwt_decode(localStorage.jwtToken);
+  const decoded = jwt_decode(localStorage.jwtToken);
   //Set User and isAuthenticated
-   store.dispatch(setCurrentUser(decoded));
+  store.dispatch(setCurrentUser(decoded));
 
-   //Check for Expired Token
-   const currentTime = Date.now() / 1000;
-   if(decoded.exp < currentTime){
-     //Logout User
-     store.dispatch(logoutUser()); 
-     //TODO : Clear current profile
-     // Redirect to Login
-     window.location.href = '/login';
-
-   }
+  //Check for Expired Token
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    //Logout User
+    store.dispatch(logoutUser());
+    // Clear current profile
+    store.dispatch(clearCurrentProfile());
+    // Redirect to Login
+    window.location.href = "/login";
+  }
 }
 
-
 class App extends Component {
-
   render() {
     return (
-      <Provider store = {store}>
-      <Router>
-      <div className="App">
-        <Navbar/> 
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
 
-       <Route exact path="/" component = {Landing} />
-       <div className="container">
-         <Route exact path = "/register" component = {Register}/>
-         <Route exact path = "/login" component = {Login}/> 
-       </div>
-      
-      <Footer />
-      </div>
-      </Router>
+            <Route exact path="/" component={Landing} />
+            <div className="container">
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/dashboard" component={Dashboard} />
+            </div>
+
+            <Footer />
+          </div>
+        </Router>
       </Provider>
     );
   }
-} 
+}
 
 export default App;
