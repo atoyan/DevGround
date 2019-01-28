0a4f84de74d1791053fd1b3993051cbed68211e6
WC ando <atoyanandranik@gmail.com>
2019-01-28 13:46:03 +0200

    Added Form Field Component

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import TextFieldGroup from "../common/TextFieldGroup";
+import TextAreaFieldGroup from "../common/TextAreaFieldGroup";
+import InputGroup from "../common/InputGroup";
+import SelectListGroup from "../common/SelectListGroup";
 
 class CreateProfile extends Component {
   constructor(props) {
@@ -32,7 +35,7 @@ class CreateProfile extends Component {
             <div className="col-md-8 m-auto">
               <h1 className="display-4 text-center">Create Your Profile</h1>
               <p className="lead text-center">
-                Let's get soem information to make you profile stand out
+                Let's get some information to make you profile stand out
               </p>
               <small className="d-block pb-3">* = required fields</small>
             </div>
@@ -53,7 +56,4 @@ const mapStateTopProps = state => ({
   errors: state.errors
 });
 
-export default connect(
-  null,
-  mapStateTopProps
-)(CreateProfile);
+export default connect(mapStateTopProps)(CreateProfile);
