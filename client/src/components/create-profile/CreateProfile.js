510c7cb6a700d5d13c1f6e8db14ba6734242637c
WC ando <atoyanandranik@gmail.com>
2019-01-29 13:30:30 +0200

    Create Profile Functionality

--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
+import { withRouter } from "react-router-dom";
 import TextFieldGroup from "../common/TextFieldGroup";
 import TextAreaFieldGroup from "../common/TextAreaFieldGroup";
 import InputGroup from "../common/InputGroup";
 import SelectListGroup from "../common/SelectListGroup";
+import { createProfile } from "../../actions/profileActions";
 
 class CreateProfile extends Component {
   constructor(props) {
@@ -28,9 +30,31 @@ class CreateProfile extends Component {
     };
   }
 
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
+    }
+  }
+
   onSubmit = e => {
     e.preventDefault();
-    console.log("submit");
+    const profileData = {
+      handle: this.state.handle,
+      company: this.state.company,
+      website: this.state.website,
+      location: this.state.location,
+      status: this.state.status,
+      skills: this.state.skills,
+      githubusername: this.state.githubusername,
+      bio: this.state.bio,
+      twitter: this.state.twitter,
+      facebook: this.state.facebook,
+      linkedin: this.state.linkedin,
+      youtube: this.state.youtube,
+      instagram: this.state.instagram
+    };
+
+    this.props.createProfile(profileData, this.props.history);
   };
   onChange = e => {
     this.setState({ [e.target.name]: e.target.value });
@@ -155,7 +179,7 @@ class CreateProfile extends Component {
                   info="City"
                 />
                 <TextFieldGroup
-                  placeholder="Skills"
+                  placeholder="* Skills"
                   name="skills"
                   value={this.state.skills}
                   onChange={this.onChange}
@@ -181,6 +205,7 @@ class CreateProfile extends Component {
 
                 <div className="mb-3">
                   <button
+                    type="button"
                     onClick={() => {
                       this.setState(prevState => ({
                         displaySocialInputs: !prevState.displaySocialInputs
@@ -218,4 +243,7 @@ const mapStateTopProps = state => ({
   errors: state.errors
 });
 
-export default connect(mapStateTopProps)(CreateProfile);
+export default connect(
+  mapStateTopProps,
+  { createProfile }
+)(withRouter(CreateProfile));
--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -37,9 +37,9 @@ class Dashboard extends Component {
 
     return (
       <div className="dashboard">
-        <div class="container">
-          <div class="row">
-            <div class="col-md-12">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
               <h1 className="display-4">Dashboard</h1>
               {dashboardContent}
             </div>
