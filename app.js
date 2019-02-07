a95d01637fa034106493f9769bbc90759516e2f4
WC ando <atoyanandranik@gmail.com>
2019-02-07 14:31:59 +0200

    Set a static folder if production

--- app.js
+++ app.js
@@ -1,53 +1,59 @@
 //DEPENDENCIES
-const express           = require("express");
-      app               = express();
-      mongoose          = require("mongoose");
-      bodyParser        = require('body-parser');
-      passport          = require('passport');
-
+const express = require("express");
+app = express();
+mongoose = require("mongoose");
+bodyParser = require("body-parser");
+passport = require("passport");
+path = require("path");
 
 //Body Parser Middleware
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //ROUTES
-const users   = require("./routes/api/users");
-const posts   = require("./routes/api/posts");
+const users = require("./routes/api/users");
+const posts = require("./routes/api/posts");
 const profile = require("./routes/api/profile");
 
-
-//PORT OR DEV ENVIRONMENT    
-const port    = process.env.PORT || 3000 ;
-
+//PORT OR DEV ENVIRONMENT
+const port = process.env.PORT || 3000;
 
 //DB CONFIG
 const db = require("./config/keys").mongoURI;
 
 //DB CONNECTION
 mongoose
-    .connect(db , { useNewUrlParser: true })
-    .then(()=>console.log("MongoDB Connected"))
-    .catch(err=>console.log(err));
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
 
+//USE ROUTES
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
-    //USE ROUTES
-    app.use('/api/users', users);
-    app.use('/api/profile', profile);
-    app.use('/api/posts', posts);
+// Server static assets if in production
 
-    //PASSPORT Middleware
-    app.use(passport.initialize());
+if (process.env.NODE_ENV === "production") {
+  // SET A STATIC FOLDER
+  app.use(express.static("client/build"));
 
-    // Passport Config
-    require('./config/passport')(passport);
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
+//PASSPORT Middleware
+app.use(passport.initialize());
 
-      //SERVER INITIALIZATION
-      app.listen(port,err=>{
-          if(err){
-              console.log(err)
-          }
-          else{
-              console.log(`The Server Running on Port ${port}`);
-          }
-      })
+// Passport Config
+require("./config/passport")(passport);
+
+//SERVER INITIALIZATION
+app.listen(port, err => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`The Server Running on Port ${port}`);
+  }
+});
